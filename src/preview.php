9960b7884f883b333dfbe9e6d47b5bcce61c3d74
jannes <jannes.hp@t-online.de>
2018-01-16 01:39:55 +0100

    key + mouse listener

--- src/preview.php
+++ src/preview.php
@@ -29,7 +29,7 @@
 <body>
 
 <!-- Preview Section -->
-<section class="section--preview" style="width: 100% !important;height: 100% !important">
+<section class="section--preview" style="width: 100% !important;height: 100% !important" onclick="eval_click(event)">
     <div id="previewRenderer" style="margin: 10px 30px;" class="full-page"></div>
     <div id="help" style="opacity: 0;"></div>
 </section>
@@ -37,6 +37,13 @@
 
 <script type="text/javascript">
     let songJson;
+
+    document.body.onkeydown = function(event){
+        if(event.keyCode === 39 || event.keyCode === 40)
+            scroll_down();
+        else if(event.keyCode === 37 || event.keyCode === 38)
+            scroll_up();
+    };
 
     function preview() {
         generate(false);
@@ -48,7 +55,6 @@
     async function update(){
         while(true){
             callAjax('/get_tmp.php?filename=<?php echo $_GET['id'];?>', function(res){
-                console.log(res);
                document.getElementById("previewRenderer").innerHTML = res;
             });
             await sleep(1000);
@@ -57,6 +63,54 @@
     function sleep(ms){
         return new Promise(resolve => setTimeout(resolve, ms));
     }
+
+    function scroll_up(){
+        let height = (document.getElementsByClassName('section--preview')[0]).offsetHeight * 0.75;
+        scrollTo(document.getElementsByClassName('section--preview')[0],
+            (document.getElementsByClassName('section--preview')[0]).scrollTop - height,
+            400);
+    }
+    function scroll_down(){
+        let height = (document.getElementsByClassName('section--preview')[0]).offsetHeight * 0.75;
+        scrollTo(document.getElementsByClassName('section--preview')[0],
+            (document.getElementsByClassName('section--preview')[0]).scrollTop + height,
+            400);
+    }
+    function eval_click(e){
+        let total_width = (document.getElementsByClassName('section--preview')[0]).offsetWidth;
+        if(e.clientX < total_width/2)
+            scroll_up();
+        else
+            scroll_down();
+    }
+
+    function scrollTo(element, to, duration) {
+        let start = element.scrollTop,
+            change = to - start,
+            currentTime = 0,
+            increment = 20;
+
+        let animateScroll = function(){
+            currentTime += increment;
+            let val = Math.easeInOutQuad(currentTime, start, change, duration);
+            element.scrollTop = val;
+            if(currentTime < duration) {
+                setTimeout(animateScroll, increment);
+            }
+        };
+        animateScroll();
+    }
+
+    //t = current time
+    //b = start value
+    //c = change in value
+    //d = duration
+    Math.easeInOutQuad = function (t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
+        t--;
+        return -c/2 * (t*(t-2) - 1) + b;
+    };
 
     update();
 </script>
