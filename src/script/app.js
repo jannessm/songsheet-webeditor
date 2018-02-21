9034c6d58e2bcfcfd4b2af2db23ea621055a6f09
jannes <jannes.hp@t-online.de>
2018-02-21 16:09:20 +0100

    preparation

--- src/script/app.js
+++ src/script/app.js
@@ -1,5 +1,29 @@
-let app = angular.module('Songsheet', []);
+let app = angular.module('songsheet', []);
+app.run(function($rootScope){
+    $rootScope.content = '';
+    $rootScope.user = {
+        name: {
+            firstname: undefined,
+            lastname: undefined,
+            fullname: function(){
+                return this.firstname + ' ' + this.lastname;
+            }
+        },
+        dbkey: undefined,
+        defaultPath: undefined
+    };
+    
+    //TODO: get cookie / check login status
+    //if cookie exists fill user
 
-let ss_ctrl = app.controller('ss_ctrl', function($scope){
 
+    //set view: if logged in -> browser
+    // else -> login
+    let file = '/view/login.html';
+    if($rootScope.user.dbkey !== undefined){
+        file = '/view/browser.html';
+    }
+    $http.get(file).then(function(response){
+        $rootScope.content = response.data;
+    });
 });
